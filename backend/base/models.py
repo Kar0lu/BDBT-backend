88904e4bfd241ee0d28eb1cd008f439270c5fd41
Kar0lu <karolugod@gmail.com>
2025-01-06 05:36:12 +0100

    feat: implement car images function

--- backend/base/models.py
+++ backend/base/models.py
@@ -31,6 +31,7 @@ class Car(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     availability = models.BooleanField(default=True)
     description = models.CharField(max_length=1000, null=True, blank=True)
+    image = image = models.ImageField(upload_to='car_images/', null=True, blank=True)
     model = models.ForeignKey('Model', on_delete=models.CASCADE)
     saloon = models.ForeignKey('Saloon', on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
